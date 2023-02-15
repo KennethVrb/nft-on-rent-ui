a00824914b0165e4c2e29fd85d7e851a4c975a18
Kenneth Verbeure <kenneth.verbeure@biscglobal.com>
2023-02-15 22:23:59 +0100

    unequip an item if user will equip a similar item

--- src/components/Inventory/EquipItem.js
+++ src/components/Inventory/EquipItem.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Button } from 'semantic-ui-react'
 import { useSubstrateState } from '../../substrate-lib'
+import { getImage } from '../../utils'
 
 export default function EquipItem({
   collectible,
@@ -20,6 +21,23 @@ export default function EquipItem({
 
     const signInfo = await getSignInfo()
 
+    const equippedCollectibleImagesMap = equippedCollectibles.map(
+      equippedCollectible => ({
+        uniqueId: equippedCollectible,
+        image: getImage(equippedCollectible),
+      })
+    )
+
+    const foundEquippedCollectible = equippedCollectibleImagesMap.find(
+      equipped =>
+        equipped.image.split('_')[0] ===
+        getImage(collectible.uniqueId).split('_')[0]
+    )
+
+    if (foundEquippedCollectible) {
+      await onUnequipItem(foundEquippedCollectible.uniqueId, false)
+    }
+
     api.tx.palletRent
       .equipCollectible(collectible.uniqueId)
       .signAndSend(...signInfo, ({ status }) => {
@@ -34,23 +52,26 @@ export default function EquipItem({
       })
   }
 
-  const onUnequipItem = async () => {
-    setLoading(true)
-
-    const signInfo = await getSignInfo()
-
-    api.tx.palletRent
-      .unequipCollectible(collectible.uniqueId)
-      .signAndSend(...signInfo, ({ status }) => {
-        if (status.isInBlock) {
-          getEquippedCollectibles()
-          setLoading(false)
-        }
+  const onUnequipItem = (uniqueId = null, useLoading = true) => {
+    useLoading && setLoading(true)
+    return new Promise((resolve, reject) => {
+      getSignInfo().then(signInfo => {
+        api.tx.palletRent
+          .unequipCollectible(uniqueId ? uniqueId : collectible.uniqueId)
+          .signAndSend(...signInfo, ({ status }) => {
+            if (status.isInBlock) {
+              getEquippedCollectibles()
+              useLoading && setLoading(false)
+              resolve()
+            }
+          })
+          .catch(e => {
+            console.log(e)
+            useLoading && setLoading(false)
+            reject()
+          })
       })
-      .catch(e => {
-        console.log(e)
-        setLoading(false)
-      })
+    })
   }
 
   useEffect(
